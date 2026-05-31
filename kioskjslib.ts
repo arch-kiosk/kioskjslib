d2fd7fc61824c0e6e3f537f98d93d636e47244bf
kioskadmin <38838314+urapadmin@users.noreply.github.com>
2026-05-31 14:55:40 +0200

    latest changes

--- kioskjslib.ts
+++ kioskjslib.ts
@@ -1,4 +1,12 @@
-export * as kioskstandardlib from "./src/kioskstandardlib"
-export * as kioskdatetime from "./src/kioskdatetime"
-export * as luxon from "luxon"
-export { KioskTimeZones } from "../kiosktsapplib";
+export * as kioskstandardlib from "./src/kioskstandardlib";
+export * as kioskdatetime from "./src/kioskdatetime";
+export * as luxon from "luxon";
+export {
+    KioskTimeZones,
+    KioskApi,
+    API_STATE_ERROR,
+    API_STATE_INITIALIZING,
+    API_STATE_READY,
+    API_STATE_UNINITIALZED,
+    FetchException,
+} from "@arch-kiosk/kiosktsapplib";
